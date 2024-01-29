b43fdf54bd540b11d15f89c51a5bc7a7e9e7c663
Rashed <rashedulhoqueceo@gmail.com>
2024-01-29 13:57:05 +0600

    create register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,57 +1,62 @@
-import { NavLink} from "react-router-dom";
+import { NavLink, useNavigate} from "react-router-dom";
 import loginImg from "../../assets/Images/Key-amico.png"
+import { useContext } from 'react';
+import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { updateProfile } from "firebase/auth";
+import auth from "../../firebase";
 
 const Register = () => {
 
-    // const {createUser} = useContext(AuthContext);
+    const {createUser} = useContext(AuthContext);
 
-    // const Navigate = useNavigate();
+    const Navigate = useNavigate();
 
-    // const handelCreateUser = e => {
-    //     e.preventDefault();
-    //     const from = e.target;
-    //     const name = from.name.value;
-    //     const email = from.email.value;
-    //     const password = from.password.value;
-    //     const photo = from.photo.value;
+    const handelCreateUser = e => {
+        e.preventDefault();
+        const from = e.target;
+        const name = from.name.value;
+        const email = from.email.value;
+        const password = from.password.value;
+        const photo = from.photo.value;
 
-    //     if (!/^(?=.*[A-Z])(?=.*[@$!%*?&]).{6,}$/.test(password)
-    //     ) {
-    //         Swal.fire({
-    //             icon: 'error',
-    //             title: "Password must be 6 characters, one uppercase and one spacial characters",
-    //             showConfirmButton: false,
-    //             timer: 2500
-    //         });
-    //         return;
-    //     }
+        if (!/^(?=.*[A-Z])(?=.*[@$!%*?&]).{6,}$/.test(password)
+        ) {
+            Swal.fire({
+                icon: 'error',
+                title: "Password must be 6 characters, one uppercase and one spacial characters",
+                showConfirmButton: false,
+                timer: 2500
+            });
+            return;
+        }
 
-    //     createUser(email, password, name, photo)
-    //         .then(res => {
-    //             updateProfile(auth.currentUser, {
-    //                 displayName: `${name}`, photoURL: `${photo}`
-    //             })
-    //             console.log(res.user)
-    //             Swal.fire({
-    //                 icon: 'success',
-    //                 title: 'Registration successful',
-    //                 showConfirmButton: false,
-    //                 timer: 2500
-    //             })
-    //             Navigate('/')
+        createUser(email, password, name, photo)
+            .then(res => {
+                updateProfile(auth.currentUser, {
+                    displayName: `${name}`, photoURL: `${photo}`
+                })
+                console.log(res.user)
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Registration successful',
+                    showConfirmButton: false,
+                    timer: 2500
+                })
+                Navigate('/')
                 
-    //         })
-    //         .catch(err => {
-    //             Swal.fire({
-    //                 icon: 'error',
-    //                 title: `${err.message}`,
-    //                 showConfirmButton: false,
-    //                 timer: 2500
-    //             })
-    //         })
+            })
+            .catch(err => {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${err.message}`,
+                    showConfirmButton: false,
+                    timer: 2500
+                })
+            })
 
 
-    // }
+    }
 
     return (
         <div className="hero container mx-auto px-2 md:px-6 py-10 md:py-24">
@@ -61,7 +66,7 @@ const Register = () => {
                 </div>
                 <div className="card w-full shadow-2xl md:w-5/12 bg-base-100 p-6 sm:p-8">
                     <h2 className="text-2xl font-bold mb-6">Create an Account</h2>
-                    <form className="space-y-4 md:space-y-6">
+                    <form onSubmit={handelCreateUser} className="space-y-4 md:space-y-6">
                         <div>
                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
                             <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Your Name" required />
